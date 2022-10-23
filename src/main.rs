1fcdd95b4dc2735b092fba8f690a5e4c0fb67c82
ArcticPheenix <phoenix.ckelly@gmail.com>
2022-10-23 07:30:35 -0600

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use yew::prelude::*;
+
+enum Msg {
+    AddOne,
+    AddTwo,
+}
+
+struct Model {
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { value: 0 }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                // The value has changed so we need to re-render for it
+                // to appear on the page.
+                true
+            }
+            Msg::AddTwo => {
+                self.value += 2;
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        // This gives us a component's "`Scope`" which allows us to send messages, etc
+        // to the component.
+        let link = ctx.link();
+        html! {
+            <div>
+                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+                <button onclick={link.callback(|_| Msg::AddTwo)}>{ "+2" }</button>
+                <p>{self.value}</p>
+            </div>
+        }
+    }
+}
+
+fn main() {
+    yew::start_app::<Model>();
+}
