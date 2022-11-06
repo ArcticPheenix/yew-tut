787d2c33069192bad709b38068e411fbcdcb0d03
ArcticPheenix <phoenix.ckelly@gmail.com>
2022-11-06 07:10:51 -0700

    More componentizing

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,40 @@ struct Video {
 #[derive(Properties, PartialEq)]
 struct VideosListProps {
     videos: Vec<Video>,
+    on_click: Callback<Video>,
 }
 
 #[function_component(VideosList)]
-fn videos_list(VideosListProps { videos }: &VideosListProps) -> Html {
+fn videos_list(VideosListProps { videos, on_click }: &VideosListProps) -> Html {
+    let on_click = on_click.clone();
     videos
         .iter()
         .map(|video| {
+            let on_video_select = {
+                let on_click = on_click.clone();
+                let video = video.clone();
+                Callback::from(move |_| on_click.emit(video.clone()))
+            };
             html! {
-                <p>{format!("{}: {}", video.speaker, video.title)}</p>
+                <p on_click={on_video_select}>{format!("{}: {}", video.speaker, video.title)}</p>
             }
         })
         .collect()
+}
+
+#[derive(Clone, Properties, PartialEq)]
+struct VideoDetailsProps {
+    video: Video,
+}
+
+#[function_component(VideoDetails)]
+fn video_details(VideoDetailsProps { video }: &VideoDetailsProps) -> Html {
+    html! {
+        <div>
+            <h3>{ video.title.clone() }</h3>
+            <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
+        </div>
+    }
 }
 
 #[function_component(App)]
@@ -53,13 +75,25 @@ fn app() -> Html {
             url: "https://youtu.be/PsaFVLr8t4E".to_string(),
         },
     ];
+
+    let selected_video = use_state(|| None);
+    let on_video_select = {
+        let selected_video = selected_video.clone();
+        Callback::from(move |video: Video| selected_video.set(Some(video)))
+    };
+    let details = selected_video.as_ref().map(|video| {
+        html! {
+            <VideoDetails video={video.clone()} />
+        }
+    });
     html! {
         <>
             <h1>{ "RustConf Explorer" }</h1>
             <div>
                 <h3>{ "Videos to watch" }</h3>
-                <VideosList videos={videos}/>
+                <VideosList videos={videos} on_click={on_video_select.clone()}/>
             </div>
+            { for details }
         </>
     }
 }
